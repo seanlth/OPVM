c0ac1f669f44163e86900505bdb8a23e7548a466
hargados <hargados@tcd.ie>
2014-12-21 00:12:43 +0000

    a

--- OPVM_Compiler/Parser.h
+++ OPVM_Compiler/Parser.h
@@ -97,11 +97,11 @@ private:
     std::string instructionPushToString(Instruction instruction);
     std::string instructionPopToString(Instruction instruction);
 
-    Parser& operator=(const Parser&);
-
 public:
     Parser();
     Parser(std::string input, std::string output);
+    Parser(Parser&);
+    void operator= (Parser&);
 
     
     void parse();
